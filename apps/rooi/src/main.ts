fd879b5d3f4a8b5d27806923a270f64b7526cc54
maratreason <maratreason@mail.ru>
2024-10-28 13:39:11 +0300

    feat(styling) add PrimeVue

--- apps/rooi/src/main.ts
+++ apps/rooi/src/main.ts
@@ -1,8 +1,16 @@
-import './styles.scss';
+// import "./styles.scss";
 
-import { createApp } from 'vue';
-import App from './app/App.vue';
+import {createApp} from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
+
+import App from "./app/App.vue";
 
 const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 
-app.mount('#root');
+app.mount("#root");
