719606eaa29059d4f8e64832168d26b8d06c6681
maratreason <maratreason@mail.ru>
2024-10-28 13:39:28 +0300

    feat(pages) add Home Page

---
+++ libs/rooi/pages/src/index.ts
@@ -0,0 +1,5 @@
+import HomePage from "./lib/Home.vue";
+
+export {
+    HomePage,
+}
---
+++ libs/rooi/pages/src/lib/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+
+// defineProps<{}>()
+name: "HomePage";
+</script>
+
+<template>
+    <p>Welcome to Home!</p>
+    <Button label="Submit" />
+</template>
+
+<style scoped></style>
